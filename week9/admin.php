5f6cd72ac1ccf0ac3fb3291ad618cf89531459e7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-09 23:08:39 +0700

    feat: Add admin.php file to insert data into admin table

---
+++ week9/admin.php
@@ -0,0 +1,12 @@
+<?php //Insert Data admin
+include ('connections.php');
+
+$username = "admin";
+$password = sha1("admin");
+$query = "INSERT INTO admin VALUES ('','$username','$password')";
+$hasil = mysqli_query($conn, $query);
+if (!$hasil) {
+  die("Gagal mengisi table admin");
+} else {
+  echo "Table <b>admin</b> berhasil mengisi";
+} ?>
