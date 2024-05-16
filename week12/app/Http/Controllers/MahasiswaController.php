9eb3820f5b798fbcd3a2e81941d035202e00fcd1
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-16 09:27:25 +0700

    init: inital commit

---
+++ week12/app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return view('mahasiswas');
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    return view('formCreate');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Mahasiswa $mahasiswa)
+  {
+    return view('mahasiswa');
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Mahasiswa $mahasiswa)
+  {
+    return view('formEdit');
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Mahasiswa $mahasiswa)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Mahasiswa $mahasiswa)
+  {
+    //
+  }
+}
--- week12/routes/web.php
+++ week12/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'show']);
+Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+Route::get('/mahasiswa/{mahasiswa}/edit', [MahasiswaController::class, 'edit']);
+Route::put('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'update']);
+Route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'destroy']);
+
